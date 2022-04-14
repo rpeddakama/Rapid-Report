dd9fe0f6f3b4a3c7b0673cf1e70971df5e919b09
rpeddakama <rishipeddakama@gmail.com>
2022-04-14 15:00:11 -0700

    data fetched is being graphed

--- client/src/components/widgets/Graph.js
+++ client/src/components/widgets/Graph.js
@@ -13,54 +13,9 @@ import {
   ComposedChart,
 } from "recharts"
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-]
-var data2 = []
-
 const Graph = ({ topic }) => {
   const [vals, setVals] = useState()
+  const [data, setData] = useState()
 
   useEffect(() => {
     const requestOptions = {
@@ -77,11 +32,10 @@ const Graph = ({ topic }) => {
         console.log("RES", res)
         parseData(res)
       })
-    // .then(() => console.log("finished"))
-    // .then(() => parseData())
   }, [])
 
   const parseData = (values) => {
+    var data2 = []
     console.log("PARSED0", values)
     if (!values) return
     console.log("PARSED", values)
@@ -93,74 +47,49 @@ const Graph = ({ topic }) => {
       x /= values[i].length
       data2.push({ date: i, score: x })
     }
-    console.log(data2[0]["score"])
+    console.log(data2)
+    setData(data2)
   }
 
-  //   console.log("VALUES", values, data[0].name)
-  //   if (values) {
-  // console.log(values.output)
-  // data[0].name = values.output.dates[0]
-  // console.log(data)
-  //   }
   const testKey = [
-    { keys: "a", score: 2 },
-    { keys: "x", score: 4 },
-    { keys: "y", score: 5 },
+    { date: 0, score: -0.7284 },
+    { date: 1, score: -0.277 },
+    { date: 2, score: 0.13288 },
+    { date: 3, score: -0.05064 },
+    { date: 4, score: -0.045139 },
+    { date: 5, score: -0.045139 },
+    { date: 6, score: -0.16502 },
   ]
+
   return (
     <div>
-      <ComposedChart
-        width={500}
-        height={300}
-        data={testKey}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="keys" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="score"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-        {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-        {/* <Bar dataKey="pv" barSize={10} fill="#413ea0" /> */}
-      </ComposedChart>
-      {/* <h1>{vals && data2[0]["score"]}</h1> */}
+      <h1>{/* HELLO {data.length} and {vals[0].length} */}</h1>
+      {data && (
+        <ComposedChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="score"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </ComposedChart>
+      )}
     </div>
-    // <ComposedChart
-    //   width={500}
-    //   height={300}
-    //   data={data}
-    //   margin={{
-    //     top: 5,
-    //     right: 30,
-    //     left: 20,
-    //     bottom: 5,
-    //   }}
-    // >
-    //   <CartesianGrid strokeDasharray="3 3" />
-    //   <XAxis dataKey="name" />
-    //   <YAxis />
-    //   <Tooltip />
-    //   <Legend />
-    //   <Line
-    //     type="monotone"
-    //     dataKey="pv"
-    //     stroke="#8884d8"
-    //     activeDot={{ r: 8 }}
-    //   />
-    //   {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-    //   <Bar dataKey="pv" barSize={10} fill="#413ea0" />
-    // </ComposedChart>
   )
 }
 
