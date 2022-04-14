d6c771ba8aafcd48b3550f4815957a220760d6f3
rpeddakama <rishipeddakama@gmail.com>
2022-04-13 22:14:05 -0700

    finish vader analysis

---
+++ server/vader.ts
@@ -0,0 +1,24 @@
+const vader = require("vader-sentiment")
+
+export async function sentimentAnalysis(sentimentInput: string[]) {
+  let res = []
+  for (var inp in sentimentInput) {
+    const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(inp)
+    res.push({
+      text: inp,
+      sentiment:
+        intensity.compound > 0.5
+          ? "positive"
+          : intensity.compound < 0.5
+          ? "negative"
+          : "neutral",
+      score: intensity.compound,
+    })
+  }
+  return res
+}
+
+// const input = 'VADER is very smart, handsome, and funny';
+// const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
+// console.log(intensity);
+// {neg: 0.0, neu: 0.299, pos: 0.701, compound: 0.8545}
