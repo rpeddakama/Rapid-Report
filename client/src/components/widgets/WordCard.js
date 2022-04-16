3f6e760afab8ba92a234130d4267011ffecd9b14
rpeddakama <rishipeddakama@gmail.com>
2022-04-15 20:37:48 -0700

    word card with color background based on sentiment working

--- client/src/components/widgets/WordCard.js
+++ client/src/components/widgets/WordCard.js
@@ -9,35 +9,26 @@ const WordCard = ({ analysis }) => {
   const sentiment = analysis[Object.keys(analysis)[0]]
   const word = Object.keys(analysis)[0]
 
-  console.log("WORD CARD ANALYSIS: ", Object.keys(analysis))
+  console.log("WORD CARD ANALYSIS: ", word, sentiment)
 
   const getColor = () => {
     if (sentiment === undefined) return "#fff"
-    if (sentiment > 0.5) return "#5ce805"
-    if (sentiment < -0.5) return "#f02416"
+    if (sentiment > 0.2) return "#5ce805"
+    if (sentiment < -0.2) return "#f02416"
     else return "#ffdd00"
   }
 
-  //   setWord(Object.keys(analysis)[0])
-  //   setSentiment(analysis[word])
   return (
-    // <div>
-    //   <h1>
-    //     {word} has a sentiment of
-    //     {sentiment}
-    //   </h1>
-    //   <h1>hello</h1>
-    // </div>
     <Card>
-      <Link to={`/problems/${file}`}>
-        <CardActionArea>
-          <CardContent>
-            <Typography ent="p">
-              <Box bgcolor="#000">{getDifficulty(file)}</Box>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Link>
+      <CardActionArea>
+        <CardContent>
+          <Typography ent="p">
+            <Box bgcolor={getColor()}>
+              {word}; sentiment: {sentiment}
+            </Box>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
