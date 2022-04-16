8ca3755459ab6ae64486e99d1cce7ced825f3da0
rpeddakama <rishipeddakama@gmail.com>
2022-04-15 20:31:41 -0700

    start card implementation

--- client/src/components/widgets/WordCard.js
+++ client/src/components/widgets/WordCard.js
@@ -1,3 +1,10 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  Box,
+} from "@material-ui/core"
 const WordCard = ({ analysis }) => {
   const sentiment = analysis[Object.keys(analysis)[0]]
   const word = Object.keys(analysis)[0]
@@ -7,13 +14,24 @@ const WordCard = ({ analysis }) => {
   //   setWord(Object.keys(analysis)[0])
   //   setSentiment(analysis[word])
   return (
-    <div>
-      <h1>
-        {word} has a sentiment of
-        {sentiment}
-      </h1>
-      <h1>hello</h1>
-    </div>
+    // <div>
+    //   <h1>
+    //     {word} has a sentiment of
+    //     {sentiment}
+    //   </h1>
+    //   <h1>hello</h1>
+    // </div>
+    <Card>
+      <Link to={`/problems/${file}`}>
+        <CardActionArea>
+          <CardContent>
+            <Typography ent="p">
+              <Box bgcolor="#000">{getDifficulty(file)}</Box>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+    </Card>
   )
 }
 
