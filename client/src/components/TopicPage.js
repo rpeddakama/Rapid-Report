492a9ec94678778bc20b8258a29f3c0da45d41a9
rpeddakama <rishipeddakama@gmail.com>
2022-04-15 20:23:18 -0700

    word card and topic page synced

--- client/src/components/TopicPage.js
+++ client/src/components/TopicPage.js
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router"
 import Graph from "./widgets/Graph"
+import WordCard from "./widgets/WordCard"
 
 const TopicPage = () => {
   const { topic } = useParams()
+
+  const [wordAnalysis, setWordAnalysis] = useState([])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: topic,
+      }),
+    }
+    fetch("http://localhost:10000/topicWordSearch", requestOptions)
+      .then((data) => data.json())
+      .then((res) => {
+        setWordAnalysis(res)
+        console.log("Word Analysis RES", res)
+      })
+  }, [])
+
   console.log("CURRENT TOPIC IS: " + topic)
   return (
     <div>
       <Graph topic={topic} />
+      {wordAnalysis.map((analysis) => (
+        <WordCard analysis={analysis} />
+      ))}
     </div>
   )
 }
--- client/src/components/widgets/WordCard.js
+++ client/src/components/widgets/WordCard.js
@@ -1,7 +1,18 @@
-const WordCard = () => {
+const WordCard = ({ analysis }) => {
+  const sentiment = analysis[Object.keys(analysis)[0]]
+  const word = Object.keys(analysis)[0]
+
+  console.log("WORD CARD ANALYSIS: ", Object.keys(analysis))
+
+  //   setWord(Object.keys(analysis)[0])
+  //   setSentiment(analysis[word])
   return (
     <div>
-      <h1>test</h1>
+      <h1>
+        {word} has a sentiment of
+        {sentiment}
+      </h1>
+      <h1>hello</h1>
     </div>
   )
 }
