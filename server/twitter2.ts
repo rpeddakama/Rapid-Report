4e5f13a9a22f41c64264e17c2ee0db016ecf2d94
rpeddakama <rishipeddakama@gmail.com>
2022-04-16 18:14:19 -0700

    counts for last 7 days

--- server/twitter2.ts
+++ server/twitter2.ts
@@ -5,9 +5,23 @@ import { TwitterApi } from "twitter-api-v2"
 // Instanciate with desired auth type (here's Bearer v2 auth)
 const client = new TwitterApi(process.env.twitter_bearer_token)
 
-export const getTweetCountV2 = async (keyword) => {
-  var params = {}
-  let res = []
-  const recentTweetsWithNode = await client.v2.tweetCountRecent(keyword)
-  console.log(recentTweetsWithNode)
+export const getTweetCountV2 = async (keyword, start, end) => {
+  let start_time = `${start}T00:00:00Z`,
+    end_time = ""
+  if (end === "") {
+    console.log("WE ARE HERE, ", start)
+    end_time = new Date(Date.now() - 30000).toISOString()
+    console.log(end_time)
+  } else end_time = `${end}T23:59:59Z`
+
+  console.log(start_time, end_time)
+  var params = {
+    start_time: start_time,
+    end_time: end_time,
+  }
+
+  const recentTweetsWithNode = await client.v2.tweetCountRecent(keyword, params)
+  console.log(recentTweetsWithNode.meta)
+
+  return recentTweetsWithNode.meta.total_tweet_count
 }
