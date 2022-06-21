258b0fd468047aee8924c85e2e2d2efd0d4afad1
rpeddakama <rishipeddakama@gmail.com>
2022-06-21 11:16:32 -0700

    uuid package + finish firebase func

--- client/src/components/services/firebase.js
+++ client/src/components/services/firebase.js
@@ -1,4 +1,20 @@
 import "firebase/auth"
 import "firebase/firestore"
-import firebase from "firebase/app"
-import { useEffect, useState } from "react"
+import admin from "firebase-admin"
+import { v4 as uuidv4 } from "uuid"
+
+const serviceAccount = require("../constants/firebasekey.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const auth = admin.auth()
+const firestore = admin.firestore()
+
+export const addToNewsletter = async (name, email) => {
+  return await firestore
+    .collection("users")
+    .doc(uuidv4())
+    .set({ name: name, email: email })
+}
